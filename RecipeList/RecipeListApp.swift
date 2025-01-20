fee8a079839e015203b7ab8c0b0175eab5f5c6a9
billvivino <76108269+billvivino@users.noreply.github.com>
2025-01-20 15:43:21 -0500

    Initial Commit

---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Bill Vivino on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeListTests/RecipeListTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListTests.swift
+//  RecipeListTests
+//
+//  Created by Bill Vivino on 1/20/25.
+//
+
+import Testing
+@testable import RecipeList
+
+struct RecipeListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
